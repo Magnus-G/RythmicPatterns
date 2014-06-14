0b3051ede9f564ae5183e1ce57dd31b2b814ec4f
Magnus Gyllenswärd <korrespondera@gmail.com>
2014-06-14 13:48:48 -0400

    Subtractions and Additions and Gates work

--- RythmicPatterns.ino
+++ RythmicPatterns.ino
@@ -1,45 +1,42 @@
+// Written by: Magnus Gyllensward
+
 #include "drumPatterns.h";
 
 const int clkIn = 2;           // the digital (clock) input
 const int digPin[2] = {3,4};  // the digital output pins
 const int pinOffset = 5;       // the first DAC pin (from 5-12)
 const int trigTime = 10;       // triggers are 10 ms.
-
-int const noOfDrumPrograms = 1;
-int const noOfColumns = 16;
-int const noOfRows = 16;
-
-//  variables for interrupt handling of the clock input
 volatile int clkState = LOW;
-int loops = 0;
+// int loops = 0;
+
+////////////////////////////////////////////////////////////
 
 // randomization/probability
-int randValue = 1023;
+int randValueSubtract = 1023;
+int randValueAdd = 1023;
+
+////////////////////////////////////////////////////////////
 
 // for the on/off switch on clock in
 int inputIsHigh = 0;
 int digState[2] = {LOW, LOW};  // start with both set low
 
+////////////////////////////////////////////////////////////
+
 // counting drumloops. see drums.h for all drum patterns
 int drumLoops = 0;
 int drumDivision[6] = {1, 2, 4, 8, 16, 32};    
-
-int anslag[noOfColumns] = {1,1,1,1,1,1,1};
-int anslagEveryOther[noOfColumns] = {1,1,1,1,1,1,1};
-
-int thereWillBeATrigger = 0;
 
 int sixth = 0;
 int seventh = 0;
 int eighth = 0;
 
-////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
 
-int gateOpenOnThisRow[noOfRows] = {1, 1, 1, 1, 1};
+int isThisATrigger[noOfDrumSteps] = {1,1,1,1,1,1,1};
+int anslagEveryOther[noOfDrumSteps] = {1,1,1,1,1,1,1};
 
-////////////////////////////////////////////////
-
-//  ==================== start of setup() ======================
+////////////////////////////////////////////////////////////
 
 void setup() {
   Serial.begin(9600);
@@ -62,59 +59,57 @@ void setup() {
   attachInterrupt(0, isr, RISING);
 }
 
-//  ==================== start of loop() =======================
+////////////////////////////////////////////////////////////
+
 void loop() {  
-          
-    // go through drum matrix
-    for (int row=0; row<noOfRows; row++) { // temporal. start with first beat point...
-      for (int column=0; column<noOfColumns; column++) { // vertical, outputs. start with output 0...
+            
+  // go through drum matrix
+  for (int column=1; column<17; column++) { // temporal. start with first beat point...
+    for (int row=1; row<noOfDrumSteps; row++) { // vertical, outputs. start with output 0...
 
-          // set drumProgram
-          int drumProgram = (analogRead(0) / (1023/noOfDrumPrograms)); if (drumProgram > 0) {  drumProgram--; } // deal with zero indexing on addressing the array vs the integer declared to set the number.
+      // will the program run for this column?
+      randValueSubtract = random(0, 1023);
+      if (randValueSubtract > analogRead(2)) {
+        
+        // set drumProgram
+        int drumProgram = (analogRead(0) / (1023/noOfDrumPrograms)); if (drumProgram > 0) {  drumProgram--; } // deal with zero indexing on addressing the array vs the integer declared to set the number.
+        
+        // the hit
+        if (drums[drumProgram][row-1][column-1] == 1) {
+          digitalWrite(pinOffset+(row-1), 1);
+        }
 
-          // if pattern shows a hit. open gate
-          if (drums[drumProgram][column][row] == 1) {
-            thereWillBeATrigger = 1;
+        // the 1 or 0 from the pattern is added to isThisATrigger
+        isThisATrigger[row-1] = drums[drumProgram][row-1][column-1]; // give isThisATrigger a 1 or 0 depending on hit or not
+
+        // a 1 is added to isThisATrigger anyway... maybe
+        randValueAdd = random(0, 1023);
+        if (randValueAdd < analogRead(3)) {
+          digitalWrite(pinOffset+(row-1), 1);
+          isThisATrigger[row-1] = 1;
+        }
+        
+        // check if gate should be turned off or kept open
+        if (isThisATrigger[row-1] == 1) { // if this is a hit
+          if (anslagEveryOther[row-1] == 1) { // if the indicator variable shows 1
+            digitalWrite(pinOffset+(row-1), LOW);  // turn gate off
+            digitalWrite(pinOffset+(row-1), HIGH);  // turn gate on 
+            digitalWrite(pinOffset+(row-1), LOW);  // turn gate off
+            anslagEveryOther[row-1] = 0; // indicator value set to 0 to indicate that the last hit turned gate off... so next one should keep it on and not go through this loop
           }
-
-          // Set random numbers for potential addition
-          int randValueAdditions = random(0, 1023);
-          if (randValueAdditions < analogRead(2)) {   
-            thereWillBeATrigger = 1;
+          else {
+            anslagEveryOther[row-1] = 1; // so that next time there will be a turning off 
           }
-
-          // Set random numbers for potential subtraction
-          int randValueSubtractions = random(0, 1023);
-          if (randValueSubtractions < analogRead(3)) {   
-            thereWillBeATrigger = 0;
-          }
-
-          // TRIGGER
-
-          if (thereWillBeATrigger == 1) {
-            digitalWrite(pinOffset + column, 0);
-            digitalWrite(pinOffset + column, 1);
-            thereWillBeATrigger = 0;
-
-            if (gateOpenOnThisRow[row] == 1) {
-              digitalWrite(pinOffset + row, 0);
-              gateOpenOnThisRow[row] = 0;
-            }
-
-            else {
-              gateOpenOnThisRow[row] = 1;
-            }
-          }
-        } // gone through all columns
-        delay(1023 - analogRead(1)); // delay after each beat == bpm
+        }         
       }
+    }
+    delay(1023 - analogRead(1)); // delay after each beat == bpm
+  }
       
+  // extra hits, outside of the drum program
   drumLoops++;
     
-
-
   // controls the on/off for the sixth output
-
   if (drumLoops % 2 == 0) {
     digitalWrite(pinOffset+(5), HIGH);
     delay(1);
@@ -129,7 +124,6 @@ void loop() {
   }
   
   // controls the on/off for the seventh output
-
   if (drumLoops % 4 == 0) {
     digitalWrite(pinOffset+(6), HIGH);
     delay(1);
@@ -144,7 +138,6 @@ void loop() {
   }  
   
   // controls the on/off for the eighth output
-
   if (drumLoops % 5 == 0) {
     digitalWrite(pinOffset+(7), HIGH);
     delay(1);
@@ -157,15 +150,6 @@ void loop() {
       eighth = 1;
     }
   }  
-
-  
-  
-  
-  
-  
-  
-  
-  
 } // end loop
 
 //  =================== convenience routines ===================
